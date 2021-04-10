917f3162ef44521895eb1bfb06d1fa8145194843
s21isilsefunc <isilsefunc@sabanciuniv.edu>
2021-04-10 23:06:48 +0300

    login arka plan eklendi

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:social_media_app/screens//walkthrough.dart';
+import 'package:social_media_app/screens/login-screen.dart';
+import 'package:social_media_app/screens/create-new-account.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: WalkThrough(),
+    home: LoginScreen(),
   ));
 }
