ee707d0ac1225b8ea03989fc0737cac00186172d
goktugcaliskan <goktugcaliskan@sabanciuniv.edu>
2021-04-24 23:31:35 +0300

    search screen final(ish?)

---
+++ lib/widgets/tag_card.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:social_media_app/utils/pallete.dart';
+import 'package:social_media_app/utils/styles.dart';
+
+class TagCard extends StatelessWidget{
+  final String imagePath;
+  final String tag;
+  final double width;
+  final double cardHeight;
+  final double imageHeight;
+  final double cardElevation;
+  final GestureTapCallback onTap;
+
+  TagCard({
+    this.imagePath = "assets/images/hashtag.png",
+    this.tag,
+    this.width = 380,
+    this.cardHeight = 60,
+    this.imageHeight = 50,
+    this.cardElevation = 4,
+    this.onTap,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        width: width,
+        height: cardHeight,
+        child: Card(
+          elevation: cardElevation,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(4),
+          ),
+          child: Row(
+            children: [
+              SizedBox(width: 10),
+              Image(image: AssetImage(imagePath), height: 25,),
+              SizedBox(width: 5),
+              Text(tag, style: TextStyle(fontSize: 18, fontStyle: FontStyle.italic),),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
